363f1e2039eea0653a4b903a7a44f099fb93ae3b
kl3jvi <klejvisiper@gmail.com>
2022-03-23 17:31:37 +0100

    finishing details page

--- app/src/main/java/com/kl3jvi/payconiq/presentation/details/DetailsFragment.kt
+++ app/src/main/java/com/kl3jvi/payconiq/presentation/details/DetailsFragment.kt
@@ -1,21 +1,46 @@
 package com.kl3jvi.payconiq.presentation.details
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.navArgs
 import com.kl3jvi.payconiq.R
 import com.kl3jvi.payconiq.common.viewBinding
 import com.kl3jvi.payconiq.databinding.DetailsFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment(R.layout.details_fragment) {
     private val viewModel: DetailsViewModel by viewModels()
     private val binding: DetailsFragmentBinding by viewBinding()
+    private val userDetails: DetailsFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        getUserData()
+    }
 
+    private fun getUserData() {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.userDetails.collect { userDetails ->
+                        Log.e("Data", userDetails.toString())
+
+                        userDetails?.let {
+                            Log.e("Data", it.toString())
+                            binding.userDetails = it
+                            binding.executePendingBindings()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/kl3jvi/payconiq/presentation/details/DetailsViewModel.kt
+++ app/src/main/java/com/kl3jvi/payconiq/presentation/details/DetailsViewModel.kt
@@ -1,11 +1,19 @@
 package com.kl3jvi.payconiq.presentation.details
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kl3jvi.payconiq.common.Resource
 import com.kl3jvi.payconiq.common.STATE_HANDLE_KEY
+import com.kl3jvi.payconiq.domain.model.UserDetails
 import com.kl3jvi.payconiq.domain.model.UserSearchedData
 import com.kl3jvi.payconiq.domain.use_case.GetUserDetailsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,10 +26,38 @@ class DetailsViewModel @Inject constructor(
         state.get<UserSearchedData>(STATE_HANDLE_KEY)?.githubUsername.orEmpty()
 
     init {
-        getUserDetailsUseCase(username = "test")
+        Log.e("git", githubUsernamePassed)
+    }
+
+    private val _userDetails = MutableStateFlow<UserDetails?>(null)
+    val userDetails: StateFlow<UserDetails?> = _userDetails
+
+    private val _error = MutableStateFlow<String?>(null)
+    val error: StateFlow<String?> = _error
+
+
+    private val _loadingState = MutableStateFlow<Boolean>(false)
+    val loadingState: StateFlow<Boolean> = _loadingState
+
+
+    init {
+        getUserDetails()
     }
 
     private fun getUserDetails() {
+        viewModelScope.launch(Dispatchers.IO) {
+            getUserDetailsUseCase(githubUsernamePassed).collect { result ->
+                when (result) {
+                    is Resource.Error -> {
+                        Log.e("Error", result.message.toString())
+                    }
+                    is Resource.Loading -> {}
+                    is Resource.Success -> {
+                        _userDetails.value = result.data
+                    }
+                }
+            }
+        }
 
     }
 }
